bac115c7b9c884129ef6f587036f36d2b581dbf2
MeryVillamil <92825889+MeryVillamil@users.noreply.github.com>
2021-10-28 20:58:09 -0500

    Add files via upload

---
+++ Decameron/src/main/java/Room34/Decameron/interfaces/InterfaceCategoria.java
@@ -0,0 +1,17 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Room34.Decameron.interfaces;
+
+import Room34.Decameron.modelo.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Administrador
+ */
+public interface InterfaceCategoria extends CrudRepository<Categoria,Integer>{
+
+    
+}
---
+++ Decameron/src/test/java/Room34/Decameron/DecameronApplicationTests.java
@@ -0,0 +1,13 @@
+package Room34.Decameron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecameronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
